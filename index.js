caa23c661c95fd772b5e4e7db6ec5ee6d4338a94
Rami Lajmi <lajmirami@yahoo.fr>
2017-01-30 00:09:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -26,51 +26,11 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-		 if (event.postback) {
-          processPostback(event);
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Bonjour XXX, Je suis un bot crée par Rami" /*+ event.message.text*/});
         }
-		res.sendStatus(200);
-		  function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.postback.payload;
-
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      sendMessage(senderId, {text: message});
-    });
-  }
-}
-		
-		
-		
-		
-		
-		
-		
-		
-        //if (event.message && event.message.text) {
-            //sendMessage(event.sender.id, {text: "Bonjour XXX, Je suis un bot crée par Rami" /*+ event.message.text*/});
-        //}
     }
-    
+    res.sendStatus(200);
 });
 
 // generic function sending messages
